e1c38af4dc92fe3b832baf966c5347d943b44f5c
MatteoMicozzi <micozzimatteo@yahoo.it>
2021-02-24 17:31:24 +0000

    fixed movement of shuttle when closer to the edges & added after-burn thrust graphic

--- app.js
+++ app.js
@@ -1,29 +1,40 @@
 var spaceAttack = document.getElementById("spaceAttack");
-var shuttle = document.getElementById("shuttle")
-var shuttlePosition
+var shuttle = document.getElementById("shuttle");
+var afterBurnRx = document.getElementById("afterBurnRx")
+var afterBurnLx = document.getElementById("afterBurnLx")
+var shuttlePosition;
+var keyRight = 'up';
+var keyLeft = 'up';
 
 setInterval(function() {
-  shuttlePosition = window.getComputedStyle(shuttle).getPropertyValue("left");
-  shuttle.style.setProperty("left", shuttlePosition);
-},20);
+  shuttlePosition = parseInt(window.getComputedStyle(shuttle).getPropertyValue("left"));
+  if (keyRight == 'down' && shuttlePosition <= 1320) {
+    shuttle.style.setProperty("left", (shuttlePosition + 4) + "px" );
+  } else if (keyLeft == 'down' && shuttlePosition >= 20) {
+    shuttle.style.setProperty("left", (shuttlePosition - 4) + "px" );
+  }
+},5);
+
 
 document.addEventListener("keydown", function(event) {
   if (event.keyCode == 39) {
-    
-    shuttle.classList.add("turnRight");
+    keyRight = 'down';
+    afterBurnLx.classList.add("afterBurnLx");
   } else if (event.keyCode == 37) {
-    shuttle.classList.add("turnLeft");
+    keyLeft = 'down';
+    afterBurnRx.classList.add("afterBurnRx");
   } else if (event.keyCode == 32) {
   }
 })
 
 
 document.addEventListener("keyup", function(event) {
-  console.log(shuttlePosition)
-  if (event.keyCode == 37) {
-    shuttle.classList.remove("turnLeft");
-  }
   if (event.keyCode == 39) {
-    shuttle.classList.remove("turnRight");
+    keyRight = 'up';
+    afterBurnLx.classList.remove("afterBurnLx");
+  }
+  if (event.keyCode == 37) {
+    keyLeft = 'up';
+    afterBurnRx.classList.remove("afterBurnRx");
   }
 })
