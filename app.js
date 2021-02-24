f53eca25fc19f633451fc98cef88bc7f8bd0059d
MatteoMicozzi <micozzimatteo@yahoo.it>
2021-02-24 23:17:16 +0000

    added shuttle blueLaser a refined functionality

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 var spaceAttack = document.getElementById("spaceAttack");
 var shuttle = document.getElementById("shuttle");
-var afterBurnRx = document.getElementById("afterBurnRx")
-var afterBurnLx = document.getElementById("afterBurnLx")
+var afterBurnRx = document.getElementById("afterBurnRx");
+var afterBurnLx = document.getElementById("afterBurnLx");
+var shuttleLaser = document.getElementById("shuttleLaser");
 var shuttlePosition;
+var shuttleLaserPositionX;
+var shuttleLaserPositionY;
+var aliensLaserPositionX;
 var keyRight = 'up';
 var keyLeft = 'up';
+var keySpacebar = 'up';
 
 setInterval(function() {
   shuttlePosition = parseInt(window.getComputedStyle(shuttle).getPropertyValue("left"));
-  if (keyRight == 'down' && shuttlePosition <= 1320) {
-    shuttle.style.setProperty("left", (shuttlePosition + 4) + "px" );
-  } else if (keyLeft == 'down' && shuttlePosition >= 20) {
-    shuttle.style.setProperty("left", (shuttlePosition - 4) + "px" );
-  }
-},5);
+  shuttleLaserPositionX = shuttlePosition + 21;
+  shuttleLaserPositionY = parseInt(window.getComputedStyle(shuttleLaser).getPropertyValue("top"));
 
+  if (keyRight == 'down' && shuttlePosition <= 1320) {
+    shuttle.style.setProperty("left", (shuttlePosition + 4) + "px");
+  };
+  if (keyLeft == 'down' && shuttlePosition >= 20) {
+    shuttle.style.setProperty("left", (shuttlePosition - 4) + "px");
+  };
+  if (keySpacebar == 'down' && shuttleLaserPositionY >= 0) {
+    shuttleLaser.style.setProperty("top", (shuttleLaserPositionY - 10) + "px");
+  } else {
+    keySpacebar = 'up';
+    shuttleLaser.style.setProperty("top", "600px");
+    shuttleLaser.classList.remove("shuttleLaser");
+  };
+},5);
 
 document.addEventListener("keydown", function(event) {
   if (event.keyCode == 39) {
@@ -24,17 +39,21 @@ document.addEventListener("keydown", function(event) {
     keyLeft = 'down';
     afterBurnRx.classList.add("afterBurnRx");
   } else if (event.keyCode == 32) {
-  }
-})
-
+    if (keySpacebar == 'up') {
+      keySpacebar = 'down';
+      shuttleLaser.style.setProperty("left", shuttleLaserPositionX + "px");
+      shuttleLaser.classList.add("shuttleLaser");
+    };
+  };
+});
 
 document.addEventListener("keyup", function(event) {
   if (event.keyCode == 39) {
     keyRight = 'up';
     afterBurnLx.classList.remove("afterBurnLx");
-  }
+  };
   if (event.keyCode == 37) {
     keyLeft = 'up';
     afterBurnRx.classList.remove("afterBurnRx");
-  }
-})
+  };
+});
